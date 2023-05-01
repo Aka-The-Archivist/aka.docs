25e57597645763cc3af9dddfee4fd20a92777e88
MwSpace llc <app@mwspace.com>
2023-05-01 18:44:24 +0200

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -241,7 +241,7 @@ export default function Home() {
                             ))}
                         </div>
                         <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
-                            &copy; 2020 Your Company, Inc. All rights reserved.
+                            &copy; 2023 Aka Backend Framework. All rights reserved.
                         </p>
                     </div>
                 </div>
